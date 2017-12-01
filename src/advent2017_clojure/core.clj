10d2993607df7d9cc27379e235cc17814677cf53
Aaron Michaels <aaronmichaels@gmail.com>
2017-12-02 01:43:44 +0700

    day 1 part 2 done

--- src/advent2017_clojure/core.clj
+++ src/advent2017_clojure/core.clj
@@ -1,13 +1,11 @@
 (ns advent2017-clojure.core
   (:gen-class))
 
-(require 'advent2017-clojure.day1.part1)
-; -------------- Day 1 ---------------
-
+(require 'advent2017-clojure.day1.solution)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (advent2017-clojure.day1.part1/solution)
+  (advent2017-clojure.day1.solution/part2)
   )
 
---
+++ src/advent2017_clojure/day1/solution.clj
@@ -0,0 +1,42 @@
+(ns advent2017-clojure.day1.solution)
+
+
+(defn- sum-equal-pairs [vector]
+  "Given a vector of pairs of chars, where the chars are ints
+  (i.e. 0-9), returns the sum of the ints in pairs with
+  identical values."
+  (->>
+    vector
+    (map (fn [[fst snd]]
+           [(Character/digit fst 10)
+            (Character/digit snd 10)]))
+    (filter (partial apply =))
+    (reduce (fn [acc [x]] (+ acc x)) 0)))
+
+
+(defn part1 []
+  (let [input (slurp "src/advent2017_clojure/day1/input.txt")
+        input-length (count input)]
+    (->>
+      input
+      clojure.string/trim
+      cycle
+      (take (inc input-length))
+      (partition 2 1)
+      (sum-equal-pairs)
+      println)))
+
+
+(defn part2 []
+  (let [input-vector
+        (vec
+          (clojure.string/trim
+            (slurp "src/advent2017_clojure/day1/input.txt")))
+        first-half-vector (subvec input-vector 0 (/ (count input-vector) 2))
+        second-half-vector (subvec input-vector (/ (count input-vector) 2))
+        first-cycle (map vector first-half-vector second-half-vector)
+        second-cycle (map vector second-half-vector first-half-vector)]
+    (println
+      (+
+        (sum-equal-pairs first-cycle)
+        (sum-equal-pairs second-cycle)))))
