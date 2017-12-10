b2e3f354970abe3a4ac3e1f00a31a4358d508dc9
Aaron Michaels <aaronmichaels@gmail.com>
2017-12-10 20:04:50 +0900

    day7 solution

--- src/advent2017_clojure/day7/solution.clj
+++ src/advent2017_clojure/day7/solution.clj
@@ -1,5 +1,4 @@
 (ns advent2017-clojure.day7.solution)
-
 (require 'clojure.set)
 
 
@@ -14,21 +13,64 @@
               [(clojure.string/split left #" ")
                (clojure.string/split right #", ")]
               [(clojure.string/split left #" ")])))
-    (mapv (fn [[[n w] xs]] {:node      n
-                            :weight    (Integer/parseInt
-                                         (subs w 1 (dec (count w))))
-                            :neighbors (if xs xs [])}))))
+    (reduce
+      (fn [acc [[node wgt] nbrs]]
+        (assoc acc node {:weight    (Integer/parseInt
+                                      (subs wgt 1 (dec (count wgt))))
+                         :neighbors (if nbrs nbrs [])}))
+      {})))
+
 
 (defn get-root [data]
   (->>
-    data
-    (reduce (fn [[node-set neighbor-set] {node      :node
-                                          weight    :weight
-                                          neighbors :neighbors}]
+    (reduce (fn [[node-set neighbor-set] node]
               [(conj node-set node)
-               (into neighbor-set neighbors)])
-            [#{} #{}])
-    ((fn [[nodes neighbors]] (clojure.set/difference nodes neighbors)))))
+               (into neighbor-set (:neighbors (data node)))])
+            [#{} #{}]
+            (keys data))
+    (apply clojure.set/difference)
+    first))
+
+
+(defn- add-total-weights [data node]
+  (let [node-weight (:weight (data node))
+        neighbors (:neighbors (data node))
+        neighbor-data (reduce #(merge (add-total-weights data %2) %1)
+                              {}
+                              neighbors)
+        neighbor-weights (reduce #(+ %1 (:total-weight %2))
+                                 0
+                                 (vals (select-keys neighbor-data neighbors)))]
+    (assoc
+      neighbor-data
+      node {:weight       node-weight
+            :neighbors    neighbors
+            :total-weight (+ neighbor-weights node-weight)})))
+
+
+(defn- odd-one-out [neighbors data]
+  (let [sorted-neighbors (vec (sort-by #(:total-weight (data %)) neighbors))
+        weight-set (set (map #(:total-weight (data %)) neighbors))]
+    (cond
+      ;; if there is no problem node return nil
+      (not= (count weight-set) 2) nil
+      ;; Else return problem node. Second node guaranteed to have
+      ;; normal weight since will be at least 3 neighbors on a broken node.
+      (= (:total-weight (second sorted-neighbors))
+         (:total-weight (first sorted-neighbors))) (last sorted-neighbors)
+      :else (first sorted-neighbors))))
+
+
+(defn- find-malformed [data node]
+  (if-let [broken-node (odd-one-out (:neighbors (data node)) data)]
+    (if-let [result (find-malformed data broken-node)]
+      result
+      (let [good-node (first (remove
+                               (partial = broken-node)
+                               (:neighbors (data node))))]
+        (list broken-node (+ (:weight (data broken-node))
+                             (- (:total-weight (data good-node))
+                                (:total-weight (data broken-node)))))))))
 
 
 (defn part1 []
@@ -37,4 +79,10 @@
     get-root
     println))
 
-(defn part2 [])
+
+(defn part2 []
+  (let [input (load-input)
+        root (get-root input)
+        data (add-total-weights input root)]
+    (println
+      (find-malformed data root))))
