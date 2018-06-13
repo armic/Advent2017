134972b8e0b1f278a94fdf57177367af925bf97a
Aaron Michaels <aaronmichaels@gmail.com>
2018-06-13 18:59:55 +0700

    Add linting deps

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.4.474"]]
+  :plugins [[lein-kibit "0.1.5"]
+            [jonase/eastwood "0.2.5"]]
   :main ^:skip-aot advent2017-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
